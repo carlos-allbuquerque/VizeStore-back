8f595ee902c1802e838a7d8a769486f8d0f16511
Carlos <carlos@pop-os.localdomain>
2023-06-06 23:42:41 -0300

    fix: login schema

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,11 +1,19 @@
 import Joi from "joi";
 import { NewUserData } from "../types/userType";
 
-export const userSchema = Joi.object<NewUserData>({
+export const newUserSchema = Joi.object<NewUserData>({
   email: Joi.string()
     .email()
     .regex(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/)
     .required(),
   password: Joi.string().min(8).required(),
   confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
+});
+
+export const userSchema = Joi.object<NewUserData>({
+  email: Joi.string()
+    .email()
+    .regex(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/)
+    .required(),
+  password: Joi.string().min(8).required(),
 });
